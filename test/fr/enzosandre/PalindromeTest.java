a81f7a9d987c7a6f94fbd7a33a3fa6544148298b
Guillaumas <guillaume.gillet@ecoles-epsi.net>
2023-12-07 17:17:24 +0100

    refacto langues palindrome

--- test/fr/enzosandre/PalindromeTest.java
+++ test/fr/enzosandre/PalindromeTest.java
@@ -2,12 +2,15 @@ package fr.enzosandre;
 
 import fr.enzosandre.test.utilities.VérificationPalindromeBuilder;
 import fr.enzosandre.utilities.PeriodeDeLaJournee;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PalindromeTest {
     @ParameterizedTest
@@ -25,32 +28,21 @@ public class PalindromeTest {
         assertTrue(résultat.contains(inversion));
     }
 
-    @Test
-    public void testPalindromeFrançais(){
-        // ETANT DONNE un palindrome
-        String palindrome = "radar";
 
-        // ET un utilisateur parlant français
-        LangueFrançaise langue = new LangueFrançaise();
-        var vérificateur = new VérificationPalindromeBuilder()
-                .AyantPourLangue(langue)
-                .Build();
-
-        // QUAND on vérifie si c'est un palindrome
-        String résultat = vérificateur.Vérifier(palindrome);
-
-        // ALORS la chaîne est répétée, suivie de "Bien dit !"
-        String attendu = palindrome + System.lineSeparator() + Expressions.BienDit;
-        assertTrue(résultat.contains(attendu));
+    static Stream<Arguments> casTestPalindrome() {
+        return Stream.of(
+                Arguments.of(new LangueAnglaise(), Expressions.WellSaid),
+                Arguments.of(new LangueFrançaise(), Expressions.BienDit)
+        );
     }
 
-    @Test
-    public void testPalindromeAnglais(){
+    @ParameterizedTest
+    @MethodSource("casTestPalindrome")
+    public void testPalindrome(LangueInterface langue, String félicitations){
         // ETANT DONNE un palindrome
         String palindrome = "radar";
 
-        // ET un utilisateur parlant anglais
-        LangueAnglaise langue = new LangueAnglaise();
+        // ET un utilisateur parlant une <langue>
         var vérificateur = new VérificationPalindromeBuilder()
                 .AyantPourLangue(langue)
                 .Build();
@@ -58,8 +50,8 @@ public class PalindromeTest {
         // QUAND on vérifie si c'est un palindrome
         String résultat = vérificateur.Vérifier(palindrome);
 
-        // ALORS la chaîne est répétée, suivie de "Well said !"
-        String attendu = palindrome + System.lineSeparator() + Expressions.WellSaid;
+        // ALORS la chaîne est répétée, suivie de félicitations dans cette langue
+        String attendu = palindrome + System.lineSeparator() + félicitations;
         assertTrue(résultat.contains(attendu));
     }
 
