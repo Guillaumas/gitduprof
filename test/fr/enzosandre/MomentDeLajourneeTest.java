f1c7bafc59d80531d3bd582a9093d1caaaac53d8
Guillaumas <guillaume.gillet@ecoles-epsi.net>
2023-12-08 15:15:59 +0100

    toutes les heures testes

--- test/fr/enzosandre/MomentDeLajourneeTest.java
+++ test/fr/enzosandre/MomentDeLajourneeTest.java
@@ -1,33 +1,32 @@
 package fr.enzosandre;
 
 import fr.enzosandre.console.DetectionMomentJournee;
+import fr.enzosandre.domain.LangueAnglaise;
+import fr.enzosandre.domain.LangueFrançaise;
+import fr.enzosandre.domain.LangueInterface;
 import fr.enzosandre.domain.PeriodeDeLaJournee;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MomentDeLajourneeTest {
-    static Stream<Arguments> casMomentDeLaJourneeSelonHeure(){
-        return Stream.of(
-                Arguments.of(0, PeriodeDeLaJournee.NUIT),
-                Arguments.of(8, PeriodeDeLaJournee.MATIN),
-                Arguments.of(11, PeriodeDeLaJournee.MATIN),
-                Arguments.of(13, PeriodeDeLaJournee.APRES_MIDI),
-                Arguments.of(17, PeriodeDeLaJournee.APRES_MIDI),
-                Arguments.of(20, PeriodeDeLaJournee.SOIREE),
-                Arguments.of(21, PeriodeDeLaJournee.SOIREE),
-                Arguments.of(23, PeriodeDeLaJournee.NUIT)
+    static Stream<Arguments> casMomentDeLaJournee(){
+        return Stream.concat(
+                IntStream.range(0, 24).mapToObj(heure -> Arguments.of(heure, new LangueAnglaise(), DetectionMomentJournee.Detecter(heure), new LangueAnglaise().getSalutation(DetectionMomentJournee.Detecter(heure)))),
+                IntStream.range(0, 24).mapToObj(heure -> Arguments.of(heure, new LangueFrançaise(), DetectionMomentJournee.Detecter(heure), new LangueFrançaise().getSalutation(DetectionMomentJournee.Detecter(heure))))
         );
     }
 
-
     @ParameterizedTest
-    @MethodSource("casMomentDeLaJourneeSelonHeure")
-    public void testMomentDeLaJourneeSelonHeure(int heure, PeriodeDeLaJournee periodeAttendue){
+    @MethodSource("casMomentDeLaJournee")
+    public void testMomentDeLaJournee(int heure, LangueInterface langue, PeriodeDeLaJournee periodeAttendue, String salutationAttendue){
         var periode = DetectionMomentJournee.Detecter(heure);
         assert periode == periodeAttendue;
-    }
 
+        var salutation = langue.getSalutation(periode);
+        assert salutation.equals(salutationAttendue);
+    }
 }
